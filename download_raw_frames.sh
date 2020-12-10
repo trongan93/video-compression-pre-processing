217dbdc8debebabf771cf7b8174d77c6078a2c08
Trong-An Bui <trongan93@gmail.com>
2020-12-10 11:44:05 +0800

    download raw frames data

--- download_raw_frames.sh
+++ download_raw_frames.sh
@@ -19,6 +19,6 @@ download_frames(){
 BASE_URL_1="https://media.xiph.org/sintel/sintel-1080-png"
 LOCAL_URL_1="${LOCAL_FRAMES_DATA_PATH}/sintel/sintel-1080-png/"
 START_ID_1=00000001
-END_ID_1=00000005
+END_ID_1=00021312
 mkdir -p "${LOCAL_URL_1}"
 download_frames "${BASE_URL_1}" "${LOCAL_URL_1}" "${START_ID_1}" "${END_ID_1}"
